727e1ea6c6acf703afc7befabfa42c814f9e49ed
devDanielFelix <danielfelix72@hotmail.com>
2024-11-17 17:24:17 -0300

    Game seed

--- src/main/java/com/devtoday/dslist/entities/Game.java
+++ src/main/java/com/devtoday/dslist/entities/Game.java
@@ -20,24 +20,28 @@ public class Game {
 	
 	@Column(name = "game_year")
 	private Integer year;
-	private String gender;
+	private String genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game () {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String gender, String platforms, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		
 		this.id = id;
 		this.title = title;
 		this.year = year;
-		this.gender = gender;
+		this.genre = genre;
 		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
@@ -70,11 +74,11 @@ public class Game {
 	}
 
 	public String getGender() {
-		return gender;
+		return genre;
 	}
 
-	public void setGender(String gender) {
-		this.gender = gender;
+	public void setGender(String genre) {
+		this.genre = genre;
 	}
 
 	public String getPlatforms() {
